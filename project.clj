7fad7ef73a01fc3c931e81ca7570c1304bdbe072
NicMcPhee <mcphee@morris.umn.edu>
2016-01-28 22:23:57 -0600

    Updated Clojure version
    For some reason the midje project maker sets the Clojure version to
    1.4.0, but LightTable requires at least 1.5.1. I bumped it all the way
    up to 1.8.0 since I was here, though.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject simple-search "0.0.1-SNAPSHOT"
   :description "Cool new project to do things and stuff"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [me.raynes/fs "1.4.6"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]]}})
 
