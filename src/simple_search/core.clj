0b4b2299297ad1890847e2643fed68c3f58ca62c
NicMcPhee <mcphee@morris.umn.edu>
2016-02-01 12:53:08 -0600

    Got most of `random-answer` implemented.
    Still need to compute the total weight and value of an answer, but that
    won't take long.

--- src/simple_search/core.clj
+++ src/simple_search/core.clj
@@ -5,3 +5,38 @@
 
 knapPI_13_20_1000_1
 
+;;; An answer will be a map with (at least) four entries:
+;;;   * :instance
+;;;   * :choices - a vector of 0's and 1's indicating whether
+;;;        the corresponding item should be included
+;;;   * :total-weight - the weight of the chosen items
+;;;   * :total-value - the value of the chosen items
+
+(defn included-items
+  "Takes a sequences of items and a sequence of choices and
+  returns the subsequence of items corresponding to the 1's
+  in the choices sequence."
+  [items choices]
+  (map first
+       (filter #(= 1 (second %))
+               (map vector items choices))))
+
+(defn random-answer
+  "Construct a random answer for the given instance of the
+  knapsack problem."
+  [instance]
+  (let [choices (repeatedly (count (:items instance))
+                            #(rand-int 2))
+        included (included-items (:items instance) choices)]
+    {:instance instance
+     :choices choices
+     :total-weight 0
+     :total-value 0}))
+
+;;; It might be cool to write a function that
+;;; generates weighted proportions of 0's and 1's.
+
+; (repeatedly 10 #(rand-int 2))
+
+(random-answer knapPI_13_20_1000_1
+)
